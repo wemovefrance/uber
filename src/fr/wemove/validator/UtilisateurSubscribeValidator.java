ca02cd64463377462829e47f2c6faebac8e5de97
wemovefrance <wemove.france@gmail.com>
2017-04-14 13:44:50 +0200

    mdpvalidation

--- src/fr/wemove/validator/UtilisateurSubscribeValidator.java
+++ src/fr/wemove/validator/UtilisateurSubscribeValidator.java
@@ -1,10 +1,16 @@
 package fr.wemove.validator;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import fr.wemove.model.Utilisateur;
+
+
+
 
 public class UtilisateurSubscribeValidator implements Validator {
 
@@ -20,26 +26,46 @@ public class UtilisateurSubscribeValidator implements Validator {
 		
 		ValidationUtils.rejectIfEmptyOrWhitespace(e, "prenom", "prenom.empty", "Le prénom doit être saisi");
 		ValidationUtils.rejectIfEmptyOrWhitespace(e, "nom", "nom.empty", "Le nom doit être saisi");
-		ValidationUtils.rejectIfEmptyOrWhitespace(e, "email", "email.empty", "Le email doit être saisi");
+		ValidationUtils.rejectIfEmptyOrWhitespace(e, "email", "email.empty", "L'email doit être saisi");
 		ValidationUtils.rejectIfEmptyOrWhitespace(e, "login", "login.empty", "Le login doit être saisi");
 		ValidationUtils.rejectIfEmptyOrWhitespace(e, "motDePasse", "motDePasse.empty", "Le motDePasse doit être saisi");
 		ValidationUtils.rejectIfEmptyOrWhitespace(e, "confirmation", "confirmation.empty", "Le confirmation doit être saisi");
+		
+		
+		
+		
 		/*
 		if ( utilisateur.getCondGV() != true) {
 			
 			e.rejectValue("condGV", "condGV.empty", "Merci d'accepter les conditions générales de ventes");
 		}
+		
 		*/
+		if (isValidEmailAddress(utilisateur.getEmail())) {
+			
+			e.rejectValue("email", "emailcheck", "Adresse email non valide");	
+		}
+		
+		if (utilisateur.getMotDePasse().length()<8) {	
+			e.rejectValue("motDePasse", "pwdcheck", "Le mot de passe doit faire au moins 8 caractères");	
+		}
 		
 		
 		if ( !utilisateur.getMotDePasse().equals(utilisateur.getConfirmation())) {
 		e.rejectValue("motDePasse", "pwdcheck", "Les mots de passe ne correspondent pas.");
 		
 		}
-	
 		
 	}
 	
-	
-
+	public static boolean isValidEmailAddress(String email) {
+		   boolean result = true;
+		   try {
+		      InternetAddress emailAddr = new InternetAddress(email);
+		      emailAddr.validate();
+		   } catch (AddressException ex) {
+		      result = false;
+		   }
+		   return result;
+		}
 }
